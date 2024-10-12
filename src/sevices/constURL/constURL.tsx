141db0b62d43cf1bf16e26b3e7f9bceda6495523
NghiaNghia <ngonghia2258@gmail.com>
2024-10-12 16:26:16 +0700

    Tạo 1 const chứa các url api

--- src/sevices/constURL/constURL.tsx
+++ src/sevices/constURL/constURL.tsx
@@ -1,5 +1,40 @@
 export const URL = {
+  AUTH: {
+    SIGN_UP: 'auth/sign-up',
+    SING_IN: 'auth/sign-in',
+    GET_ROLES: 'auth/get-roles',
+    GET_CURRENT_USER: '',
+    LOGOUT: '',
+    REFRESH_TOKEN: '',
+    FORGOT_PASSWORD: '',
+    RESET_PASSWORD: '',
+    UPDATE_ROLE_GROUP: 'auth/update-rolegroup',
+    GET_ROLE_GROUPS: 'auth/get-rolegroups',
+  },
   CUSTOMER: {
     GETALL: 'customer',
+    GETONE: 'customer',
+    CREATE: 'customer',
+    UPDATE: 'customer',
+    DELETE: 'customer',
+    FILTER_INCLUDE_SPENDING: 'customer/filter-include-spending',
+  },
+  PRODUCT: {
+    GETALL: 'product',
+    GETONE: 'product',
+    CREATE: 'product',
+    UPDATE: 'product',
+    DELETE: 'product',
+  },
+  PRODUCTCATEGORY: {
+    GETALL: 'productcategory',
+  },
+  ORDER: {
+    GETALL: 'order',
+    GETONE: 'order',
+    CREATE: 'order',
+    UPDATE: 'order',
+    DELETE: 'order',
+    PAYMENT: 'order/payment',
   },
 };
